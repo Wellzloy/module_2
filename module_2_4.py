ce6adc58f2fa7b4a1bd0589ff07130d8b9a82884
Wellzloy <well1970@mail.ru>
2024-11-07 23:02:56 +0400

    07.11

---
+++ module_2_4.py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []  # только простые числа
+not_primes = []  # не простые числа
+for i in numbers:
+    print(i)
+print(primes)
+print(not_primes)
