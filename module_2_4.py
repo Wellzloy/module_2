dbf00e390a2e686fd538f14637da834e5f585e16
Валентин <well1970@mail.ru>
2024-11-10 15:36:50 +0400

    10/11

--- module_2_4.py
+++ module_2_4.py
@@ -1,7 +1,23 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-primes = []  # только простые числа
-not_primes = []  # не простые числа
-for i in numbers:
-    print(i)
-print(primes)
-print(not_primes)
+primes = []
+not_primes = []
+
+for number in numbers:
+    is_prime = True
+    if number > 1:
+        for number_1 in range(2, number):
+            if (number % number_1) == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(number)
+        else:
+            not_primes.append(number)
+    else:
+        not_primes.append(number)
+
+
+print(f"Простые числа: {primes}")
+print(f"Не простые числа: {not_primes}")
+
+
