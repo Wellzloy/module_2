38af5dd91ae7cdc959f6755d7d43b82fb3e83656
Валентин <well1970@mail.ru>
2024-11-10 18:30:01 +0400

    10/11

---
+++ module_2_5.py
@@ -0,0 +1,19 @@
+def get_matrix(n, m, value):
+    matrix = []
+    inner_list = []
+    for line in range(n):
+        matrix.append(inner_list)
+    for column in range(m):
+        inner_list.append(value)
+    return matrix
+
+
+
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+
+print(result1)
+print(result2)
+print(result3)
